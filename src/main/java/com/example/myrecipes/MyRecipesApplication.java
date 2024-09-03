9fcd49c179d008940a60dc41c862fe45ce63a79f
John Rey <adioskid@mail.ru>
2024-09-03 20:50:52 +0400

    Initial commit

---
+++ src/main/java/com/example/myrecipes/MyRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.example.myrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRecipesApplication.class, args);
+	}
+
+}
