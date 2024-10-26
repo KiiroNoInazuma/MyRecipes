56e3c12a7253020d80054671e9d07f45a42b9bef
John Rey <adioskid@mail.ru>
2024-10-26 19:51:33 +0400

    #finish

--- src/main/java/com/example/myrecipes/controllers/RecipeController.java
+++ src/main/java/com/example/myrecipes/controllers/RecipeController.java
@@ -3,15 +3,19 @@ package com.example.myrecipes.controllers;
 import com.example.myrecipes.models.Recipe;
 import com.example.myrecipes.services.RecipeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalTime;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class RecipeController {
+    private static int x;
 
     private final RecipeService recipeService;
 
@@ -24,6 +28,9 @@ public class RecipeController {
     @GetMapping("/recipe/get/{recipeId}")
     public ResponseEntity<Recipe> getRecipe(@PathVariable int recipeId) {
         Recipe recipe = recipeService.getRecipe(recipeId);
+        LocalTime currentTime = LocalTime.now();
+        int currentSeconds = currentTime.getSecond();
+        log.info(String.format("test: %s, %s", x++, currentSeconds));
         return ResponseEntity.ok(recipe);
     }
 
